8b8770c805b56974733b1cd82c5df20f74ac4716
celso-rodrigo <celsorodrigoma@gmail.com>
2023-03-16 17:37:58 -0300

    feat: Criado about de UserInfo

--- src/components/UserInfoAbout.js
+++ src/components/UserInfoAbout.js
@@ -4,6 +4,20 @@ function UserInfoAbout() {
 	return (
 		<div className="p-2 px-4">
 			<h1 className="text-xl text-white font-light">Pink</h1>
+			<p 
+				className="text-xs text-white font-light pb-4"
+			>
+          Celso
+				<span className="pl-3">
+            Campina Grande, Paraiba, Brazil
+				</span>
+			</p>
+			<a 
+				href="" 
+				className="text-xs text-white font-light pb-4 hover:underline"
+			>
+        Why you have to be so amazing?
+			</a>
 		</div>
 	);
 }
